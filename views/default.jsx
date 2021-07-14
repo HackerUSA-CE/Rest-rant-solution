09f8e0f74c33b92f29503fa30186568902e11290
clayp0 <c.anders.christenson@gmail.com>
2021-07-14 17:15:23 -0500

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,31 @@
 const React = require('react')
 
-function Def (html) {
+function Def(html) {
 	return (
-	    <html>
-		<head>
-		    <title>Title</title>
-		    <link rel="stylesheet" href="/css/style.css"/>
-		    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
-		</head>
-		<body>
-		    {html.children}
-		</body>
-	    </html>
+		<html>
+			<head>
+				<title>Title</title>
+				<link rel="stylesheet" href="/css/style.css" />
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
+			</head>
+			<body>
+				<nav>
+					<ul>
+						<li>
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="/places">Places</a>
+						</li>
+						<li>
+							<a href="/places/new">Add Place</a>
+						</li>
+					</ul>
+				</nav>
+				{html.children}
+			</body>
+		</html>
 	)
-      }
+}
 
 module.exports = Def

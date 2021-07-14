fc51f91ab6367f431db68e7d711762b77ae77ec1
clayp0 <c.anders.christenson@gmail.com>
2021-07-14 17:18:19 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,31 +2,41 @@ const router = require('express').Router()
 const places = require("../models/places")
 
 router.post('/', (req, res) => {
-	console.log(req.body)
-	if (!req.body.pic) {
-	  // Default image if one is not provided
-	  req.body.pic = 'http://placekitten.com/400/400'
-	}
-	if (!req.body.city) {
-	  req.body.city = 'Anytown'
-	}
-	if (!req.body.state) {
-	  req.body.state = 'USA'
-	}
-	places.push(req.body)
-	res.redirect('/places')
-      })
+    console.log(req.body)
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
 
 
 router.get('/', (req, res) => {
-	res.render('places/index', { places })
+    res.render('places/index', { places })
 })
 
 
 router.get('/new', (req, res) => {
-	res.render('places/new')
+    res.render('places/new')
 });
 
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    } else if (!places[id]) {
+        res.render('error404')
+    } else {
+        res.render('places/show', { place: places[id] })
+    }
+})
 
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,15 +2,19 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-	let placesFormatted = data.places.map((place) => {
+	let placesFormatted = data.places.map((place, index) => {
 		return (
 		  <div className="col-sm-6">
-		    <h2>{place.name}</h2>
-		    <p clasName="text-center">
+		    <h2>
+		      <a href={`/places/${index}`} >
+			{place.name}
+		      </a>
+		    </h2>
+		    <p className="text-center">
 		      {place.cuisines}
 		    </p>
-		    <img height="300" width="500" src={place.pic} alt={place.name} />
-		    <p clasName="text-center">
+		    <img src={place.pic} alt={place.name} />
+		    <p className="text-center">
 		      Located in {place.city}, {place.state}
 		    </p>
 		  </div>
